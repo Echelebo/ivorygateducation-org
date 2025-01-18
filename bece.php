334892d96e802a85843864c9cf19aa8ad24ba403
Echelebo <echelebochibuike@gmail.com>
2025-01-18 09:55:20 +0100

    first

--- bece.php
+++ bece.php
@@ -68,7 +68,7 @@ $page_title = "BECE (Junior Waec) Class";
                             </p>
 
                             <p class="mb-20">
-                           <b> Parental Involvement</b>
+                           <b class="mt-4"> Parental Involvement</b>
 
 <ul><li>Support Your Child: Encourage a positive attitude towards studying, and create a conducive environment for learning.</li>
 <li>Stay Informed: Keep track of important dates related to registration, exams, and results.</li></ul>
