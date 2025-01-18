e001e8f34dd19b1541d5cc35a8e7b917987f47fe
Echelebo <echelebochibuike@gmail.com>
2025-01-18 09:53:55 +0100

    first

--- bece.php
+++ bece.php
@@ -1,4 +1,4 @@
-<?php
+<b?php
 $page_title = "BECE (Junior Waec) Class";
 ?>
 
@@ -30,7 +30,7 @@ $page_title = "BECE (Junior Waec) Class";
                             <div class="category-list">
                                 <a href="/sat"><i class="far fa-long-arrow-right"></i>SAT</a>
                                 <a href="/bece"><i class="far fa-long-arrow-right"></i>BECE (Junior Waec) Class</a>
-                                <nc href="/common-entrance"><i class="far fa-long-arrow-right"></i>Common Entrance Class</a>
+                                <a href="/common-entrance"><i class="far fa-long-arrow-right"></i>Common Entrance Class</a>
                                 <a href="/icttraining"><i class="far fa-long-arrow-right"></i>ICT Training</a>
                                 <a href="/jamb"><i class="far fa-long-arrow-right"></i>JAMB Class</a>
                                 <a href="/ss1-foundation"><i class="far fa-long-arrow-right"></i>SS1 Foundation Class</a>
@@ -41,92 +41,46 @@ $page_title = "BECE (Junior Waec) Class";
                         </div>
                         <div class="widget department-download">
                             <h4 class="widget-title">Free Resources</h4>
-                            <a href="#"><i class="far fa-file-pdf"></i> JAMB Forum</a>
-                            <a href="#"><i class="far fa-file-pdf"></i> SAT Forum</a>
-                            <a href="#"><i class="far fa-file-pdf"></i> SSCE Forum</a>
+                            <a href="#"><i class="far fa-file-alt"></i> JAMB Forum</a>
+                            <a href="#"><i class="far fa-file-alt"></i> SAT Forum</a>
+                            <a href="#"><i class="far fa-file-alt"></i> SSCE Forum</a>
                             <a href="/blog"><i class="far fa-file-alt"></i> Blog</a>
                         </div>
                     </div>
                 </div>
                 <div class="col-xl-8 col-lg-8">
                     <div class="department-details">
-                        <div class="department-details-img mb-30">
-                            <img src="assets/img/department/single.jpg" alt="thumb">
-                        </div>
+
                         <div class="department-details">
                             <h3 class="mb-20">BECE (JUNIOR WAEC)</h3>
                             <p class="mb-20">
-                                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
-                                doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
-                                veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
-                                voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
-                                consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque
-                                porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
-                                velit, sed quia non numquam eius modi tempora incidunt ut labore et.
+                            The Basic Education Certificate Examination (BECE), commonly referred to as Junior WAEC, is a crucial assessment for students completing their Junior High School education. It serves as a gateway for further educational opportunities and is vital for assessing student learning outcomes.
                             </p>
+
                             <p class="mb-20">
-                                But I must explain to you how all this mistaken idea of denouncing pleasure and
-                                praising pain was born and I will give you a complete account of the system, and
-                                expound the actual teachings of the great explorer of the truth, the
-                                master-builder of human happiness. No one rejects, dislikes, or avoids pleasure
-                                itself, because it is pleasure, but because those who do not know how to pursue
-                                pleasure rationally encounter consequences that are extremely painful. Nor again
-                                is there anyone who loves or pursues or desires to obtain pain of itself,
-                                because it is pain, but because occasionally circumstances occur in which toil
-                                and pain can procure him some great pleasure. To take a trivial example
+                            <b>Preparation Tips for Students</b>
+<ul>
+<li>Consistent Study Schedule: Create a timetable to cover all subjects effectively.</li>
+<li>Practice Past Questions: Familiarize yourself with the exam format and question types.</li>
+<li>Group Study: Encourage group studies to enhance understanding through discussion.</li>
+<li>Health and Well-being: Maintain a balanced diet and ensure adequate rest before the exams.</li>
+</ul>
                             </p>
-                            <div class="row">
-                                <div class="col-md-6 mb-20">
-                                    <img src="assets/img/department/01.jpg" alt="">
-                                </div>
-                                <div class="col-md-6 mb-20">
-                                    <img src="assets/img/department/02.jpg" alt="">
-                                </div>
-                            </div>
+
                             <p class="mb-20">
-                                Power of choice is untrammelled and when nothing prevents our being able to do
-                                what we like best, every pleasure is to be welcomed and every pain avoided. But
-                                in certain circumstances and owing to the claims of duty or the obligations of
-                                business it will frequently occur that pleasures have to be repudiated and
-                                annoyances accepted. The wise man therefore always holds in these matters to
-                                this principle of selection.
+                           <b> Parental Involvement</b>
+
+<ul><li>Support Your Child: Encourage a positive attitude towards studying, and create a conducive environment for learning.</li>
+<li>Stay Informed: Keep track of important dates related to registration, exams, and results.</li></ul>
                             </p>
                             <div class="my-4">
                                 <div class="mb-3">
                                     <h3 class="mb-3">How To Prepare For The Exam</h3>
-                                    <p>Aliquam facilisis rhoncus nunc, non vestibulum mauris volutpat non.
-                                        Vivamus tincidunt accumsan urna, vel aliquet nunc commodo tristique.
-                                        Nulla facilisi. Phasellus vel ex nulla. Nunc tristique sapien id mauris
-                                        efficitur, porta scelerisque nisl dignissim. Vestibulum ante ipsum
-                                        primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed at
-                                        mollis tellus. Proin consequat, orci nec bibendum viverra, ante orci
-                                        suscipit dolor, et condimentum felis dolor ac lectus.</p>
+                                    <p>The BECE is a significant milestone in a student's academic journey. Ivorygate Education center play's a pivotal role in preparing students for this examination by providing support, resources, and guidance. Let's work together to ensure every student is equipped to succeed!</p>
                                 </div>
-                                <ul class="department-single-list">
-                                    <li><i class="far fa-check"></i>Fusce justo risus placerat in risus eget
-                                        tincidunt consequat elit.</li>
-                                    <li><i class="far fa-check"></i>Nunc fermentum sem sit amet dolor laoreet
-                                        placerat.</li>
-                                    <li><i class="far fa-check"></i>Nullam rhoncus dictum diam quis ultrices.
-                                    </li>
-                                    <li><i class="far fa-check"></i>Integer quis lorem est uspendisse eu augue
-                                        porta ullamcorper dictum.</li>
-                                    <li><i class="far fa-check"></i>Quisque tristique neque arcu ut venenatis
-                                        felis malesuada et.</li>
-                                </ul>
+
                             </div>
-                            <div class="my-4">
-                                <h3 class="mb-3">Admission In Business And Finance</h3>
-                                <p>Quisque a nisl id sem sollicitudin volutpat. Cras et commodo quam, vel congue
-                                    ligula. Orci varius natoque penatibus et magnis dis parturient montes,
-                                    nascetur ridiculus mus. Cras quis venenatis neque. Donec volutpat tellus
-                                    lobortis mi ornare eleifend. Fusce eu nisl ut diam ultricies accumsan.
-                                    Integer lobortis vestibulum nunc id porta. Curabitur aliquam arcu sed ex
-                                    dictum, a facilisis urna porttitor. Fusce et mattis nisl. Sed iaculis libero
-                                    consequat justo auctor iaculis. Vestibulum sed ex et magna tristique
-                                    bibendum. Sed hendrerit neque nec est suscipit, id faucibus dolor convallis.
-                                </p>
-                            </div>
+
                         </div>
                     </div>
                 </div>
